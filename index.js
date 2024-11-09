6f8195ea884ce5dbe17542974a8a757da21443ab
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-09 11:04:31 +0800

    implemented simple express backend

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const express = require("express");
+const app = express();
+
+// make express backend run on port 3000 or localhost:3000
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node API. Location: Home");
+});
+
+app.get("/contact_us", (req, res) => {
+    res.send("Hello from Node API. Location: Contact Us Page");
+});
+
+app.get("/about_us", (req, res) => {
+    res.send("Hello from Node API. Location: About Us Page");
+});
