cc82e104ab6cab24e227c265e7b095732dba8911
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-09 14:49:54 +0800

    finished implementing most of the middleware functionalities

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Product from "./models/product.model.js";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send("Notified from Node API Server.");
@@ -33,9 +34,6 @@ app.get("/api/product/:id", async (req, res) => {
 
 app.post("/api/products", async (req, res) => {
     try {
-        // res.send("Data received").status(200);
-        // console.log(`${req.body}`);
-
         const product = await Product.create(req.body);
         res.status(200).json(product);
     } catch (error) {
@@ -57,6 +55,22 @@ app.put("/api/product/:id", async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+});
+
+app.delete("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully." });
 });
 
 mongoose
